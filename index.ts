18f2afc4307c10678beba3aeeb44b282902af1f8
Theo LECOUBLET <theo.lecoublet@uca.fr>
2026-07-02 13:16:24 +0200

    refactor: reorganize exports in index.ts for better structure and readability

--- index.ts
+++ index.ts
@@ -5,65 +5,76 @@ export { AvailableSizes, useSizeMapping, type Size, useVariantMapping, type Vari
 
 export * from './types'
 
-export { default as DuButton } from './components/Actions/du-button/du-button.vue';
-export { default as DuBadge } from './components/DataDisplay/du-badge/du-badge.vue';
-export { default as DuDropdown } from './components/Actions/du-dropdown/du-dropdown.vue';
-export { default as DuModal } from './components/Actions/du-modal/du-modal.vue';
-export { default as DuSwap } from './components/Actions/du-swap/du-swap.vue';
-export { default as DuAccordion } from './components/DataDisplay/du-accordion/du-accordion.vue';
-export { default as DuAccordionItem } from './components/DataDisplay/du-accordion/du-accordion-item.vue';
-export { default as DuAvatar } from './components/DataDisplay/du-avatar/du-avatar.vue';
-export { default as DuCard } from './components/DataDisplay/du-card/du-card.vue';
-export { default as DuCarousel } from './components/DataDisplay/du-carousel/du-carousel.vue';
-export { default as DuCarouselItem } from './components/DataDisplay/du-carousel/du-carousel-item.vue';
-export { default as DuChat } from './components/DataDisplay/du-chat/du-chat.vue';
-export { default as DuChatItem } from './components/DataDisplay/du-chat/du-chat-item.vue';
-export { default as DuCollapse } from './components/DataDisplay/du-collapse/du-collapse.vue';
-export { default as DuCountdown } from './components/DataDisplay/du-countdown/du-countdown.vue';
-export { default as DuCountdownGroup } from './components/DataDisplay/du-countdown/du-countdown-group.vue';
-export { default as DuDiff } from './components/DataDisplay/du-diff/du-diff.vue';
-export { default as DuKbd } from './components/DataDisplay/du-kbd/du-kbd.vue';
-export { default as DuList } from './components/DataDisplay/du-list/du-list.vue';
-export { default as DuListRow } from './components/DataDisplay/du-list/du-list-row.vue';
-export { default as DuStat } from './components/DataDisplay/du-stat/du-stat.vue';
-export { default as DuStats } from './components/DataDisplay/du-stats/du-stats.vue';
-export { default as DuStatus } from './components/DataDisplay/du-status/du-status.vue';
-export { default as DuTable } from './components/DataDisplay/du-table/du-table.vue';
-export { default as DuTableItem } from './components/DataDisplay/du-table/du-table-item.vue';
-export { default as DuTimeline } from './components/DataDisplay/du-timeline/du-timeline.vue';
-export { default as DuTimelineItem } from './components/DataDisplay/du-timeline/du-timeline-item.vue';
-export { default as DuCheckbox } from './components/DataInput/du-checkbox/du-checkbox.vue';
-export { default as DuFieldset } from './components/DataInput/du-fieldset/du-fieldset.vue';
-export { default as DuFileInput } from './components/DataInput/du-file-input/du-file-input.vue';
-export { default as DuFilter } from './components/DataInput/du-filter/du-filter.vue';
-export { default as DuInputField } from './components/DataInput/du-input-field/du-input-field.vue';
-export { default as DuLabel } from './components/DataInput/du-label/du-label.vue';
-export { default as DuLabelInputValidator } from './components/DataInput/du-label-input-validator/du-label-input-validator.vue';
-export { default as DuRadio } from './components/DataInput/du-radio/du-radio.vue';
-export { default as DuRange } from './components/DataInput/du-range/du-range.vue';
-export { default as DuRating } from './components/DataInput/du-rating/du-rating.vue';
-export { default as DuRatingItem } from './components/DataInput/du-rating/du-rating-item.vue';
-export { default as DuSearch } from './components/DataInput/du-search/du-search.vue';
-export { default as DuSelect } from './components/DataInput/du-select/du-select.vue';
-export { default as DuTextArea } from './components/DataInput/du-text-area/du-text-area.vue';
-export { default as DuAlert } from './components/Feedback/du-alert/du-alert.vue';
-export { default as DuLoading } from './components/Feedback/du-loading/du-loading.vue';
-export { default as DuProgress } from './components/Feedback/du-progress/du-progress.vue';
-export { default as DuRadialProgress } from './components/Feedback/du-radial-progress/du-radial-progress.vue';
-export { default as DuSkeleton } from './components/Feedback/du-skeleton/du-skeleton.vue';
-export { default as DuToast } from './components/Feedback/du-toast/du-toast.vue';
-export { default as DuTooltip } from './components/Feedback/du-tooltip/du-tooltip.vue';
-export { default as DuJoin } from './components/Layout/du-join/du-join.vue';
-export { default as DuBreadcrumbs } from './components/Navigation/du-breadcrumbs/du-breadcrumbs.vue';
-export { default as DuButtonLink } from './components/Navigation/du-button-link/du-button-link.vue';
-export { default as DuDock } from './components/Navigation/du-dock/du-dock.vue';
-export { default as DuLink } from './components/Navigation/du-link/du-link.vue';
-export { default as DuMenu } from './components/Navigation/du-menu/du-menu.vue';
-export { default as DuMenuItem } from './components/Navigation/du-menu/du-menu-item.vue';
-export { default as DuNavbar } from './components/Navigation/du-navbar/du-navbar.vue';
-export { default as DuPagination } from './components/Navigation/du-pagination/du-pagination.vue';
-export { default as DuStepItem } from './components/Navigation/du-step-item/du-step-item.vue';
-export { default as DuSteps } from './components/Navigation/du-steps/du-steps.vue';
-export { default as DuTabs } from './components/Navigation/du-tabs/du-tabs.vue';
-export { default as DuFab } from './components/Actions/du-fab/du-fab.vue';
-export { default as DuDrawer } from './components/Layout/du-drawer/du-drawer.vue';
+// Actions
+export { default as DuButton } from './components/Actions/du-button/du-button.vue'
+export { default as DuDropdown } from './components/Actions/du-dropdown/du-dropdown.vue'
+export { default as DuFab } from './components/Actions/du-fab/du-fab.vue'
+export { default as DuModal } from './components/Actions/du-modal/du-modal.vue'
+export { default as DuSwap } from './components/Actions/du-swap/du-swap.vue'
+
+// Data Display
+export { default as DuAccordion } from './components/DataDisplay/du-accordion/du-accordion.vue'
+export { default as DuAccordionItem } from './components/DataDisplay/du-accordion/du-accordion-item.vue'
+export { default as DuAvatar } from './components/DataDisplay/du-avatar/du-avatar.vue'
+export { default as DuBadge } from './components/DataDisplay/du-badge/du-badge.vue'
+export { default as DuCard } from './components/DataDisplay/du-card/du-card.vue'
+export { default as DuCarousel } from './components/DataDisplay/du-carousel/du-carousel.vue'
+export { default as DuCarouselItem } from './components/DataDisplay/du-carousel/du-carousel-item.vue'
+export { default as DuChat } from './components/DataDisplay/du-chat/du-chat.vue'
+export { default as DuChatItem } from './components/DataDisplay/du-chat/du-chat-item.vue'
+export { default as DuCollapse } from './components/DataDisplay/du-collapse/du-collapse.vue'
+export { default as DuCountdown } from './components/DataDisplay/du-countdown/du-countdown.vue'
+export { default as DuCountdownGroup } from './components/DataDisplay/du-countdown/du-countdown-group.vue'
+export { default as DuDiff } from './components/DataDisplay/du-diff/du-diff.vue'
+export { default as DuKbd } from './components/DataDisplay/du-kbd/du-kbd.vue'
+export { default as DuList } from './components/DataDisplay/du-list/du-list.vue'
+export { default as DuListRow } from './components/DataDisplay/du-list/du-list-row.vue'
+export { default as DuStat } from './components/DataDisplay/du-stat/du-stat.vue'
+export { default as DuStats } from './components/DataDisplay/du-stats/du-stats.vue'
+export { default as DuStatus } from './components/DataDisplay/du-status/du-status.vue'
+export { default as DuTable } from './components/DataDisplay/du-table/du-table.vue'
+export { default as DuTableItem } from './components/DataDisplay/du-table/du-table-item.vue'
+export { default as DuTimeline } from './components/DataDisplay/du-timeline/du-timeline.vue'
+export { default as DuTimelineItem } from './components/DataDisplay/du-timeline/du-timeline-item.vue'
+
+// Data Input
+export { default as DuCheckbox } from './components/DataInput/du-checkbox/du-checkbox.vue'
+export { default as DuFieldset } from './components/DataInput/du-fieldset/du-fieldset.vue'
+export { default as DuFileInput } from './components/DataInput/du-file-input/du-file-input.vue'
+export { default as DuFilter } from './components/DataInput/du-filter/du-filter.vue'
+export { default as DuInputField } from './components/DataInput/du-input-field/du-input-field.vue'
+export { default as DuLabel } from './components/DataInput/du-label/du-label.vue'
+export { default as DuLabelInputValidator } from './components/DataInput/du-label-input-validator/du-label-input-validator.vue'
+export { default as DuRadio } from './components/DataInput/du-radio/du-radio.vue'
+export { default as DuRange } from './components/DataInput/du-range/du-range.vue'
+export { default as DuRating } from './components/DataInput/du-rating/du-rating.vue'
+export { default as DuRatingItem } from './components/DataInput/du-rating/du-rating-item.vue'
+export { default as DuSearch } from './components/DataInput/du-search/du-search.vue'
+export { default as DuSelect } from './components/DataInput/du-select/du-select.vue'
+export { default as DuTextArea } from './components/DataInput/du-text-area/du-text-area.vue'
+
+// Feedback
+export { default as DuAlert } from './components/Feedback/du-alert/du-alert.vue'
+export { default as DuLoading } from './components/Feedback/du-loading/du-loading.vue'
+export { default as DuProgress } from './components/Feedback/du-progress/du-progress.vue'
+export { default as DuRadialProgress } from './components/Feedback/du-radial-progress/du-radial-progress.vue'
+export { default as DuSkeleton } from './components/Feedback/du-skeleton/du-skeleton.vue'
+export { default as DuToast } from './components/Feedback/du-toast/du-toast.vue'
+export { default as DuTooltip } from './components/Feedback/du-tooltip/du-tooltip.vue'
+
+// Layout
+export { default as DuDrawer } from './components/Layout/du-drawer/du-drawer.vue'
+export { default as DuJoin } from './components/Layout/du-join/du-join.vue'
+
+// Navigation
+export { default as DuBreadcrumbs } from './components/Navigation/du-breadcrumbs/du-breadcrumbs.vue'
+export { default as DuButtonLink } from './components/Navigation/du-button-link/du-button-link.vue'
+export { default as DuDock } from './components/Navigation/du-dock/du-dock.vue'
+export { default as DuLink } from './components/Navigation/du-link/du-link.vue'
+export { default as DuMenu } from './components/Navigation/du-menu/du-menu.vue'
+export { default as DuMenuItem } from './components/Navigation/du-menu/du-menu-item.vue'
+export { default as DuNavbar } from './components/Navigation/du-navbar/du-navbar.vue'
+export { default as DuPagination } from './components/Navigation/du-pagination/du-pagination.vue'
+export { default as DuStepItem } from './components/Navigation/du-step-item/du-step-item.vue'
+export { default as DuSteps } from './components/Navigation/du-steps/du-steps.vue'
+export { default as DuTabs } from './components/Navigation/du-tabs/du-tabs.vue'
